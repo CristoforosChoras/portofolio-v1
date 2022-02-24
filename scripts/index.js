bb8e6deafed988b543cfeb0cade82aecd3616d85
CristoforosChoras <christopher.choras@yahoo.com>
2022-02-24 10:06:09 +0100

    Updates

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+// Any code you will write for your website (in the future!) should go here
+let span = document.querySelector(".year");
+let year = new Date().getFullYear();
+ span.innerHTML = year;
